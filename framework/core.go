9f3ca147e3887691e34b89b1ae7dca8275521780
iceymoss <ice_moss@163.com>
2024-06-02 18:06:53 +0800

    feat: 路由注册和路由匹配

--- framework/core.go
+++ framework/core.go
@@ -11,15 +11,18 @@ import (
 type Core struct {
 	//router map[string]ControllerHandler
 	//二级哈希，一级存储对应Method，二级存储对应Method下URI
-	router map[string]map[string]ControllerHandler
+	//router map[string]map[string]ControllerHandler
+
+	//前缀树路由匹配
+	router map[string]*Tree // all routers
 }
 
 // NewCore 初始化对象Core
 func NewCore() *Core {
 	methods := []string{"GET", "POST", "PUT", "DELETE"}
-	router := make(map[string]map[string]ControllerHandler)
+	router := make(map[string]*Tree)
 	for _, method := range methods {
-		router[method] = map[string]ControllerHandler{}
+		router[method] = NewTree()
 	}
 	return &Core{router: router}
 }
@@ -28,56 +31,66 @@ func NewCore() *Core {
 func (c *Core) Get(url string, handler ControllerHandler) {
 	//大小写不敏感，增加用户容错
 	upperUrl := strings.ToUpper(url)
-	c.router["GET"][upperUrl] = handler
+	if err := c.router["GET"].AddRouter(upperUrl, handler); err != nil {
+		log.Fatal("add router error: ", err)
+	}
 }
 
 // Post POST方法路由注册
 func (c *Core) Post(url string, handler ControllerHandler) {
 	upperUrl := strings.ToUpper(url)
-	c.router["POST"][upperUrl] = handler
+	if err := c.router["POST"].AddRouter(upperUrl, handler); err != nil {
+		log.Fatal("add router error: ", err)
+	}
 }
 
 // Put PUT方法路由注册
 func (c *Core) Put(url string, handler ControllerHandler) {
 	upperUrl := strings.ToUpper(url)
-	c.router["PUT"][upperUrl] = handler
+	if err := c.router["PUT"].AddRouter(upperUrl, handler); err != nil {
+		log.Fatal("add router error: ", err)
+	}
 }
 
 // Delete DELETE方法路由注册
 func (c *Core) Delete(url string, handler ControllerHandler) {
 	upperUrl := strings.ToUpper(url)
-	c.router["DELETE"][upperUrl] = handler
+	if err := c.router["DELETE"].AddRouter(upperUrl, handler); err != nil {
+		log.Fatal("add router error: ", err)
+	}
 }
 
-// FindRouteByRequest 路由匹配算法，没有匹配返回nil
+// FindRouteByRequest 匹配路由，如果没有匹配到，返回nil
 func (c *Core) FindRouteByRequest(request *http.Request) ControllerHandler {
-	//获取Method和URI
+	// uri 和 method 全部转换为大写，保证大小写不敏感
 	uri := request.URL.Path
 	method := request.Method
-	//统一转为大写
-	upperUri := strings.ToUpper(uri)
 	upperMethod := strings.ToUpper(method)
-	//匹配Method
+
+	// 查找第一层map
 	if methodHandlers, ok := c.router[upperMethod]; ok {
-		//匹配URI并获取对应的控制器
-		if handler, ok := methodHandlers[upperUri]; ok {
-			return handler
-		}
+		return methodHandlers.FindHandler(uri)
 	}
 	return nil
 }
 
 // ServeHTTP 框架核心结构实现了Handler接口
+// 所有请求都进入这个函数, 这个函数负责路由分发
 func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	log.Println("ServeHTTP")
+
+	// 封装自定义context
 	ctx := NewContext(request, response)
-	router := c.FindRouteByRequest(ctx.request)
+
+	// 寻找路由
+	router := c.FindRouteByRequest(request)
 	if router == nil {
+		// 如果没有找到，这里打印日志
 		ctx.Json(404, "not found")
 		return
 	}
-	err := router(ctx)
-	if err != nil {
+
+	// 调用路由函数，如果返回err 代表存在内部错误，返回500状态码
+	if err := router(ctx); err != nil {
 		ctx.Json(500, "inner error")
 		return
 	}
--- framework/tree.go
+++ framework/tree.go
@@ -1,6 +1,9 @@
 package framework
 
-import "strings"
+import (
+	"errors"
+	"strings"
+)
 
 // 基于前缀树(字典树)实现动态路由匹配
 // 构造Node
@@ -11,9 +14,22 @@ type node struct {
 	childs  []*node           // 代表这个节点下的子节点
 }
 
+func newNode() *node {
+	return &node{
+		isLast:  false,
+		segment: "",
+		childs:  []*node{},
+	}
+}
+
 // Tree 前缀树
 type Tree struct {
 	root *node //根节点
+}
+
+func NewTree() *Tree {
+	root := newNode()
+	return &Tree{root}
 }
 
 // 判断一个segment是否是通用segment，即以:开头
@@ -26,10 +42,12 @@ func (n *node) filterChildNodes(segment string) []*node {
 	if len(n.childs) == 0 {
 		return nil
 	}
+
 	// 如果segment是通配符，则所有下一层子节点都满足需求
 	if isWildSegment(segment) {
 		return n.childs
 	}
+
 	nodes := make([]*node, 0, len(n.childs))
 	// 过滤所有的下一层子节点
 	for _, cnode := range n.childs {
@@ -41,5 +59,111 @@ func (n *node) filterChildNodes(segment string) []*node {
 			nodes = append(nodes, cnode)
 		}
 	}
+
 	return nodes
+}
+
+// 判断路由是否已经在节点的所有子节点树中存在了，本质是遍历前缀树
+func (n *node) matchNode(uri string) *node {
+	// 使用分隔符将uri切割为两个部分
+	segments := strings.SplitN(uri, "/", 2)
+	// 第一个部分用于匹配下一层子节点
+	segment := segments[0]
+	if !isWildSegment(segment) {
+		segment = strings.ToUpper(segment)
+	}
+	// 匹配符合的下一层子节点
+	cnodes := n.filterChildNodes(segment)
+	// 如果当前子节点没有一个符合，那么说明这个uri一定是之前不存在, 直接返回nil
+	if cnodes == nil || len(cnodes) == 0 {
+		return nil
+	}
+
+	// 如果只有一个segment，则是最后一个标记
+	if len(segments) == 1 {
+		// 如果segment已经是最后一个节点，判断这些cnode是否有isLast标志
+		for _, tn := range cnodes {
+			if tn.isLast {
+				return tn
+			}
+		}
+
+		// 都不是最后一个节点
+		return nil
+	}
+
+	// 如果有2个segment, 递归每个子节点继续进行查找
+	for _, tn := range cnodes {
+		tnMatch := tn.matchNode(segments[1])
+		if tnMatch != nil {
+			return tnMatch
+		}
+	}
+	return nil
+}
+
+// AddRouter 增加路由节点, 路由节点有先后顺序
+/*
+/book/list
+/book/:id (冲突)
+/book/:id/name
+/book/:student/age
+/:user/name
+/:user/name/:age (冲突)
+*/
+func (tree *Tree) AddRouter(uri string, handler ControllerHandler) error {
+	n := tree.root
+	if n.matchNode(uri) != nil {
+		return errors.New("route exist: " + uri)
+	}
+
+	segments := strings.Split(uri, "/")
+	// 对每个segment
+	for index, segment := range segments {
+
+		// 最终进入Node segment的字段
+		if !isWildSegment(segment) {
+			segment = strings.ToUpper(segment)
+		}
+		isLast := index == len(segments)-1
+
+		var objNode *node // 标记是否有合适的子节点
+
+		childNodes := n.filterChildNodes(segment)
+		// 如果有匹配的子节点
+		if len(childNodes) > 0 {
+			// 如果有segment相同的子节点，则选择这个子节点
+			for _, cnode := range childNodes {
+				if cnode.segment == segment {
+					objNode = cnode
+					break
+				}
+			}
+		}
+
+		if objNode == nil {
+			// 创建一个当前node的节点
+			cnode := newNode()
+			cnode.segment = segment
+			if isLast {
+				cnode.isLast = true
+				cnode.handler = handler
+			}
+			n.childs = append(n.childs, cnode)
+			objNode = cnode
+		}
+
+		n = objNode
+	}
+
+	return nil
+}
+
+// FindHandler 匹配uri
+func (tree *Tree) FindHandler(uri string) ControllerHandler {
+	matchNode := tree.root.matchNode(uri)
+	if matchNode == nil {
+		return nil
+	}
+	return matchNode.handler
 }
