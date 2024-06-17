e19e04df412da2ac1ea0b6d81ffe4b697276dae0
iceymoss <ice_moss@163.com>
2024-06-18 00:45:55 +0800

    feat: 完善context封装

--- framework/core.go
+++ framework/core.go
@@ -87,6 +87,20 @@ func (c *Core) FindRouteByRequest(request *http.Request) []ControllerHandler {
 	return nil
 }
 
+// FindRouteNodeByRequest 匹配路由，如果没有匹配到，返回nil
+func (c *Core) FindRouteNodeByRequest(request *http.Request) *node {
+	// uri 和 method 全部转换为大写，保证大小写不敏感
+	uri := request.URL.Path
+	method := request.Method
+	upperMethod := strings.ToUpper(method)
+
+	// 查找第一层map
+	if methodHandlers, ok := c.router[upperMethod]; ok {
+		return methodHandlers.root.matchNode(uri)
+	}
+	return nil
+}
+
 // ServeHTTP 框架核心结构实现了Handler接口
 // 所有请求都进入这个函数, 这个函数负责路由分发
 func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
@@ -95,17 +109,25 @@ func (c *Core) ServeHTTP(response http.ResponseWriter, request *http.Request) {
 	ctx := NewContext(request, response)
 
 	// 寻找路由
-	handlers := c.FindRouteByRequest(request)
-	if handlers == nil {
+	//handlers := c.FindRouteByRequest(request)
+	//if handlers == nil {
+	//	// 如果没有找到，这里打印日志
+	//	ctx.Json(404, "not found")
+	//	return
+	//}
+
+	// 寻找路由
+	noder := c.FindRouteNodeByRequest(request)
+	if noder == nil {
 		// 如果没有找到，这里打印日志
 		ctx.Json(404, "not found")
 		return
 	}
 
 	// 设置路由参数
-	params := node.parseParamsFromEndNode(request.URL.Path)
+	params := noder.parseParamsFromEndNode(request.URL.Path)
 	ctx.SetParams(params)
-	ctx.SetHandlers(handlers)
+	ctx.SetHandlers(noder.handlers)
 
 	// 调用路由函数，如果返回err 代表存在内部错误，返回500状态码
 	if err := ctx.Next(); err != nil {
